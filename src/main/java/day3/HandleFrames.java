96f8ba80e7b0ce4d19bd0be752c4326796a5a17f
Srihari <srihari.muruhan@gmail.com>
2017-05-10 15:14:09 +0530

    5/10/2017

--- src/main/java/day3/HandleFrames.java
+++ src/main/java/day3/HandleFrames.java
@@ -11,70 +11,38 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class HandleFrames {
 	public static void main(String[] args) throws InterruptedException {
-		//Initialize the Driver path
-		System.setProperty("webdriver.chrome.driver",
-				"./drivers/chromedriver.exe");
+		// Initialize the Driver path
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 
-		//Launch/Initialize Browser
-		//ClassName obj = new ClassName();
+		// Launch/Initialize Browser
+		// ClassName obj = new ClassName();
 		ChromeDriver driver = new ChromeDriver();
 
-		/*System.setProperty("webdriver.gecko.driver",
-				"./drivers/geckodriver.exe");
-
-		//Launch/Initialize Browser
-		//ClassName obj = new ClassName();
-		FirefoxDriver driver = new FirefoxDriver();*/
+		/*
+		 * System.setProperty("webdriver.gecko.driver",
+		 * "./drivers/geckodriver.exe");
+		 * 
+		 * //Launch/Initialize Browser //ClassName obj = new ClassName();
+		 * FirefoxDriver driver = new FirefoxDriver();
+		 */
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-		//Maximize the browser
+		// Maximize the browser
 		driver.manage().window().maximize();
 
-		//Launch the URL
+		// Launch the URL
 		driver.get("http://jqueryui.com/droppable/");
-		
-		WebElement  demo = driver.findElementByClassName("demo-frame");
-		
+
+		WebElement demo = driver.findElementByClassName("demo-frame");
+
 		driver.switchTo().frame(demo);
-		
+
 		System.out.println(driver.findElementByXPath("//*[@id='draggable']/p").getText());
-		
-		//Come out of the Frame
+
+		// Come out of the Frame
 		driver.switchTo().defaultContent();
-		
+
 		driver.findElementByLinkText("Download").click();
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 	}
 }
